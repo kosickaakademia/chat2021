26e1657cf33d53719c923fb5d8ac6fdc0e0b81bd
kosickaakademia <roland.onofrej@kosickaakademia.sk>
2021-02-11 12:44:39 +0100

    Added class Message, added new methods in Database

--- src/sk/kosickaakademia/onofrej/chat/database/Database.java
+++ src/sk/kosickaakademia/onofrej/chat/database/Database.java
@@ -1,9 +1,11 @@
 package sk.kosickaakademia.onofrej.chat.database;
 
 import sk.kosickaakademia.onofrej.chat.util.Util;
+import sk.kosickaakademia.onofrej.entity.Message;
 import sk.kosickaakademia.onofrej.entity.User;
 
 import java.sql.*;
+import java.util.List;
 
 public class Database {
     private String url = "jdbc:mysql://itsovy.sk:3306/chat2021";
@@ -11,6 +13,7 @@ public class Database {
     private String password = "Kosice2021!";
     private final String insertNewUser = "INSERT INTO user (login, password) VALUES (?,?)";
     private final String loginUser = "Select * FROM user Where login LIKE ? and password LIKE ?";
+    private final String newMessage = "INSERT INTO message( frto, to, text) VALUES (?,?,?)";
 
     private Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -94,7 +97,57 @@ public class Database {
     }
 
     public boolean changePassword(String login , String oldPassword, String newPassword){
-
+        // TO DO
         return false;
     }
+
+    public boolean sendMessage(int from, String toUser, String text){
+        if(text==null || text.equals(""))
+            return false;
+        int to = getUserId(toUser);
+        if(to==-1)
+            return false;
+        try {
+            Connection con = getConnection();
+            if(con ==null){
+                System.out.println("Connection issue!");
+                return false;
+            }
+            PreparedStatement ps = con.prepareStatement(newMessage);
+            ps.setInt(1,from);
+            ps.setInt(2, to);
+            ps.setString(3, text);
+            int result = ps.executeUpdate();
+            con.close();
+            if(result<1){
+                System.out.println("Message not delivered!");
+                return false;
+            }
+            else{
+                System.out.println("Message sent successfully!");
+                return true;
+            }
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public int getUserId(String login){
+        
+        return -1;
+    }
+
+    public List<Message> getMyMessages(String login){
+
+        // volame metodu deleteAllMyMessages
+        return null;
+    }
+
+    public void deleteAllMyMessages(String login){
+
+    }
+
+
+
 }
